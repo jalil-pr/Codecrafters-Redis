b78b7a51fe6e467b0c40a904d629f27bf963c607
Jalil <jalilhaidari53@gmail.com>
2024-03-21 16:05:57 +0000

    debug hanlding concurrent request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
@@ -16,6 +21,7 @@ public class Main {
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
     int port = 6379;
+    ExecutorService es =null;
 
     // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // ensures that we don't run into 'Address already in use' errors
@@ -25,14 +31,49 @@ public class Main {
 
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
-      while (true) {
-        clientSocket = serverSocket.accept();
-        // Thread t = new Thread();
-        Main mainClass = new Main();
-        Main.Responder res = mainClass.new Responder(clientSocket);
-        res.run();
+      es = Executors.newFixedThreadPool(4);
+      class Handler implements Runnable{
+        Socket clienSocket;
+        Handler(Socket clientSocket){
+          this.clienSocket = clientSocket;
+        }
+        @Override
+        public void run() {
+          try{
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8);
+            BufferedReader in  = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String line;
+            while (!(line = in.readLine()).isEmpty()) {
+              System.out.println("Received:"+line);
+              if("ping".equals(line) || line.contains("ping")){
+                outputStreamWriter.write("+PONG\r\n");
+                outputStreamWriter.flush();
 
+              }
+            }
+
+
+          }catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+          } finally {
+            try {
+              if (clientSocket != null) {
+                clientSocket.close();
+      
+              }
+            } catch (IOException e) {
+              System.out.println("IOException: " + e.getMessage());
+            }
+          }
+          
+        }
+        
       }
+      while ((clientSocket = serverSocket.accept())!=null) {
+        es.submit(new Handler(clientSocket));
+      }
+
+      
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
@@ -44,46 +85,6 @@ public class Main {
       } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
       }
-    }
-
-  }
-
-  class Responder implements Runnable {
-    Socket socket;
-
-    Responder(Socket socket) {
-      this.socket = socket;
-    }
-
-    @Override
-    public void run() {
-      try {
-        OutputStream outputStream = socket.getOutputStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String inputLine = bufferedReader.readLine();
-        while (inputLine != null) {
-          
-          if (inputLine.contains("ping") || inputLine.equalsIgnoreCase("ping")) {
-            outputStream.write("+PONG\r\n".getBytes());
-            outputStream.flush();
-          } // 2nd statement
-          inputLine = bufferedReader.readLine();
-          // new PrintWriter(clientSocket.getOutputStream(), true);
-        }
-        outputStream.close();
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      } finally {
-        try {
-          if (socket != null) {
-            socket.close();
-
-          }
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        }
-      }
-
     }
 
   }
